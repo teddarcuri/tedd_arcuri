2606ef77abcc7e09d28f604936e8110fde23f669
teddarcuri <tarcuri88@gmail.com>
2014-05-31 23:13:56 -0600

    Initial commit, new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeddArcuri::Application.config.secret_key_base = '18da08878a09a209201479a943a02173266caea745215e3980f63914844866d7a658b85dd91bd9351b704516ca3e5d9cea4b25c8aec52649f5807ec4e441bf1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeddArcuri::Application.config.session_store :cookie_store, key: '_tedd_arcuri_session'
